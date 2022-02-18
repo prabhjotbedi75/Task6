6709703c3168079abf0cdbaefacee41b9c5ab847
Prabhjot Singh Bedi <prabhjotbedi75@gmail.com>
2022-02-18 12:03:24 +0530

    Update gitautomate.sh

--- gitautomate.sh
+++ gitautomate.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
-git init
 git remote add proja  https://github.com/prabhjotbedi75/ProjectA.git
 git remote add projb  https://github.com/prabhjotbedi75/ProjectB.git
 git pull proja master
-git add .
-git commit -m"commit"
-git push projb master
+git push -u projb master
 
