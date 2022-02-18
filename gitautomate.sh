a8621d5748637b9671bde7831fd8f6a90729e828
Prabhjot Singh Bedi <prabhjotbedi75@gmail.com>
2022-02-18 15:44:42 +0530

    Add files via upload

---
+++ gitautomate.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+git pull  https://github.com/prabhjotbedi75/ProjectA.git
+#git checkout origin/master -- /Users/prabhjot.intern/Desktop/ProjectA/readme.txt
+git remote set-url origin https://github.com/prabhjotbedi75/ProjectB.git
+#git add README.md
+#git commit -m "New changes pushed to Project B"
+git push origin master --force
