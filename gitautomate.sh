f42be49a49d6a427dcc856dcd7b762b9817b9246
Prabhjot Singh Bedi <prabhjotbedi75@gmail.com>
2022-02-18 15:44:28 +0530

    Delete gitautomate.sh

--- gitautomate.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-git remote add proja  https://github.com/prabhjotbedi75/ProjectA.git
-git remote add projb  https://github.com/prabhjotbedi75/ProjectB.git
-git pull proja master
-git push -u projb master
-
